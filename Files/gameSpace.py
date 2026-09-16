0c16f0e7e9029733d0611d2c8b72029c1c55a2e2
Davian Alicea Aponte <da.a.aliceaaponte@baypath.net>
2026-09-16 10:48:51 -0400

    Scaled down player image + Organized things

--- Files/gameSpace.py
+++ Files/gameSpace.py
@@ -8,9 +8,11 @@ pygame.init()
 screen_width=1400
 screen_height=700
 
-shipImg = pygame.image.load('galaga-ship.png')
-x1 = 450
-y1 = 50
+shipImg = pygame.image.load('../Images/galaga-ship.png')
+shipImg_width = shipImg.get_rect().width
+shipImg_height = shipImg.get_rect().height
+x1 = 699
+y1 = 640
 
 screen = pygame.display.set_mode((screen_width,screen_height))
 
@@ -27,10 +29,11 @@ while keep_playing==True:
         if event.type == pygame.QUIT:
             keep_playing = False
 
+    shipImg = pygame.transform.scale(shipImg, (shipImg_width*0.1, shipImg_height*0.1))
     screen.blit(shipImg, (x1,y1))         
 
     pygame.display.update()
-    
+
     clock.tick(60)
 
 pygame.quit()
