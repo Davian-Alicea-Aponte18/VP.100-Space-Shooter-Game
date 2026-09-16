3c019ab92c8ea6028eedfcd4b2f6724725292a24
Davian Alicea Aponte <da.a.aliceaaponte@baypath.net>
2026-09-16 08:18:42 -0400

    Modified code slightly

--- gameSpace.py
+++ gameSpace.py
@@ -5,8 +5,8 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
 
 pygame.init()
 
-screen_width=700
-screen_height=300
+screen_width=1400
+screen_height=700
 
 shipImg = pygame.image.load('galaga-ship.png')
 x1 = 450
@@ -16,7 +16,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 
 pygame.display.set_caption("Bootleg Galaga")
 
-#screen.fill((0, 0, 0))
+screen.fill((0, 0, 0))
 
 clock = pygame.time.Clock()
 
@@ -27,10 +27,11 @@ while keep_playing==True:
         if event.type == pygame.QUIT:
             keep_playing = False
 
-screen.blit(shipImg, (x1,y1))         
+    screen.blit(shipImg, (x1,y1))         
 
-pygame.display.update()
-clock.tick(60)
+    pygame.display.update()
+    
+    clock.tick(60)
 
 pygame.quit()
 quit()
