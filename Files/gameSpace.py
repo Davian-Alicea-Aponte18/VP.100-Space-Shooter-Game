3969e1a8b76b232266a2a981ee6c905fce641a63
Davian Alicea Aponte <da.a.aliceaaponte@baypath.net>
2026-09-16 14:02:12 -0400

    Added Player class

--- Files/gameSpace.py
+++ Files/gameSpace.py
@@ -1,6 +1,29 @@
 import pygame
 
 import os
+
+class Player:
+    def __init__(self, shiprect, image):
+      self._shiprect=shiprect
+      self._image=image
+
+    def set_shiprect(self, shiprect):
+        self._shiprect=shiprect
+    def get_shiprect(self):
+        return self._shiprect
+
+    def set_image(self, image):
+        self._image=image
+    def get_image(self):
+        return self._image
+
+    def move(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT] or pressed[pygame.K_a]:
+            self._shiprect.x = self._shiprect.x - 2
+        if pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
+            self._shiprect.x = self._shiprect.x + 2
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
 
 pygame.init()
@@ -9,9 +32,11 @@ screen_width=1400
 screen_height=700
 
 shipImg = pygame.image.load('../Images/galaga-ship.png')
-shipImg_width = shipImg.get_rect().width
-shipImg_height = shipImg.get_rect().height
-x1 = 699
+ship_rect = shipImg.get_rect()
+shipImg_width = ship_rect.width
+shipImg_height = ship_rect.height
+ship=Player(ship_rect, shipImg)
+x1 = 700
 y1 = 640
 
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -29,8 +54,10 @@ while keep_playing==True:
         if event.type == pygame.QUIT:
             keep_playing = False
 
-    shipImg = pygame.transform.scale(shipImg, (shipImg_width*0.1, shipImg_height*0.1))
-    screen.blit(shipImg, (x1,y1))         
+    screen.fill((0, 0, 0))
+    ship.set_image(pygame.transform.scale(shipImg, (shipImg_width*0.1, shipImg_height*0.1)))
+    screen.blit(ship.get_image(), (x1,y1))
+    ship.move()
 
     pygame.display.update()
 
